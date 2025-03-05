f2ed70adf999d884e57ff3e0809fe3002e7e196b
NaurisJ <nauris.javalds@gmail.com>
2025-03-05 11:54:17 +0200

    Initial commit

---
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -0,0 +1,15 @@
+package lv.venta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyFirstController {
+	
+	@GetMapping("/simple") // localhost:8080/simple
+	public String myFirstGetController() {
+		System.out.println("Pirmais kontrolieris nostradaja");
+		return "simple-page"; // paradit simple-page.html lapu
+	}
+
+}
