2b40cbffb3c8f18dc0d9df5400661da85016b9d1
NaurisJ <nauris.javalds@gmail.com>
2025-03-05 12:09:25 +0200

    EDIT: added new controller for data sending

--- src/main/java/lv/venta/controller/MyFirstController.java
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -1,15 +1,28 @@
 package lv.venta.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyFirstController {
 	
+	private Random rand = new Random();
+	
 	@GetMapping("/simple") // localhost:8080/simple
 	public String myFirstGetController() {
 		System.out.println("Pirmais kontrolieris nostradaja");
 		return "simple-page"; // paradit simple-page.html lapu
+	}
+	
+	@GetMapping("/getData")
+	public String getControllerSendData(Model model) {
+		System.out.println("Send data kontrolieris nostradaja");
+		String data = "Karina -> " + rand.nextInt(0, 101);
+		model.addAttribute("package");
+		return "show-data-page"; // paradis show-data-page.html failu, kura bus jau ieveitots data vertiba
 	}
 
 }
