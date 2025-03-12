53a7c4b50d2e5214c22649e6eb475e886ebed5df
NaurisJ <nauris.javalds@gmail.com>
2025-03-12 11:11:57 +0200

    EDIT: added data jpa annotations to product model class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,11 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,15 +17,24 @@ import lombok.AccessLevel;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
-	
+	@Column(name = "Title")
 	private String title;
+	@Column(name = "Description")
 	private String description;
+	@Column(name = "Price")
 	private float price;
+	
+	@Column(name = "Quantity")
 	private int quantity;
 	
 	
