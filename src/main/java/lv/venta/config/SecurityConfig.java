a1e6345014c8ed286f3f3d60daf58cf45a6e41bd
NaurisJ <nauris.javalds@gmail.com>
2025-03-28 10:39:24 +0200

    EDIT: added securityfilterchain function in security config class

---
+++ src/main/java/lv/venta/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package lv.venta.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public UserDetailsManager createInMemoryUsers() {
+		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+		
+		UserDetails ud1 = User.builder()
+				.username("karina")
+				.password(encoder.encode("qwerty123"))
+				.authorities("USER")
+				.build();
+		
+		UserDetails ud2 = User.builder()
+				.username("admin")
+				.password(encoder.encode("1234"))
+				.authorities("ADMIN")
+				.build();
+		
+		UserDetails ud3 = User.builder()
+				.username("peteris")
+				.password(encoder.encode("0987"))
+				.authorities("USER")
+				.build();
+		
+		
+		
+		
+		InMemoryUserDetailsManager imUserDetailsMan 
+		= new InMemoryUserDetailsManager(ud1, ud2, ud3);
+		
+		return imUserDetailsMan;
+		
+		
+	}
+	
+	@Bean
+	public SecurityFilterChain createConfigForEndpoints(HttpSecurity http) {
+		http.authorizeHttpRequests().requestMatchers("/simple").permitAll()
+			.requestMatchers("/getdata").hasAuthority("ADMIN").requestMatchers("/getmultipleproducts").hasAuthority("ADMIN")
+			.requestMatchers("/prduct/crud/all").permitAll()
+			.requestMatchers("/product/crud/one**").permitAll()
+			.requestMatchers("/produc/crud/all/**").permitAll()
+			.requestMatchers("/product/crud/create").hasAuthority("ADMIN")
+			.requestMatchers("/product/crud/update/**").hasAuthority("ADMIN")
+			.requestMatchers("/product/crud/delete/**").hasAuthority("ADMIN")
+			.requestMatchers("/product/process/price/**").hasAnyAuthority("ADMIN","USER");
+		
+		http.formLogin().permitAll();
+		
+		return http.build();
+	}
+	
+	
+
+}
